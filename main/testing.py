1dddc03a31b4fb3b630e4eb66c4c11eddfa7e3b4
EnderBenjy <benyoh73@yahoo.com>
2022-06-01 19:35:14 +0200

    Bunch of tests

--- main/testing.py
+++ main/testing.py
@@ -20,11 +20,22 @@ net = Network([784,32,32,10])
 net.SGD(training_data, 20, 10, 3.0, test_data=test_data)
 print(f'\n \n \n \n Resultats: {final}')
 '''
-for i in range(5):
-    print(f"net1{i} = Network([784, {i*10+10}, 10])")
 
-for i in range(5):
-    print(f"net2{i} = Network([784, {i*10+10}, {i*10+10}, 10])")
+nb_neurones = [8,16,32]
+nb_layers = [0,1,2,5]
 
-for i in range(5):
-    print(f"net3{i} = Network([784, {i*10+10}, {i*10+10}, {i*10+10}, 10])")
+net3 = Network(784,10)
+net4 = Network(784,8,10)
+net5 = Network(784,16,10)
+net6 = Network(784,32,10)
+net7 = Network(784,8,8,10)
+net8 = Network(784,16,16,10)
+net9 = Network(784,32,32,10)
+net10 = Network(784,8,8,8,8,8,10)
+net11 = Network(784,16,16,16,16,16,10)
+net12 = Network(784,32,32,32,32,32,10)
+
+reseaux = [net3, net4, net5, net6, net7, net8, net9, net10, net11, net12]
+
+for network in reseaux:
+    network.SGD(training_data, 10, 10, 3.0, test_data=test_data)
